62e465f3cfe4bf72768c04a1421e61ff972b9838
kuma0112 <lsj23871@gmail.com>
2024-11-19 17:16:24 +0900

    chore: global, domain 폴더 분리

---
+++ backend/src/main/java/com/meong9/backend/domain/member/Member.java
@@ -0,0 +1,4 @@
+package com.meong9.backend.domain.member;
+
+public class Member {
+}
--- backend/src/main/java/com/meong9/backend/global/S3Config.java
+++ backend/src/main/java/com/meong9/backend/global/S3Config.java
@@ -1,4 +1,4 @@
-package com.meong9.backend.config;
+package com.meong9.backend.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
