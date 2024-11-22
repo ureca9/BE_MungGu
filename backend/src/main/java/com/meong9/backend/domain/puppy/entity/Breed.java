a747a2c9143c5f7082206958655e892bf109988e
kuma0112 <lsj23871@gmail.com>
2024-11-22 13:11:13 +0900

    refactor: id -> breedId 변경 및 boolean -> Boolean

--- backend/src/main/java/com/meong9/backend/domain/puppy/entity/Breed.java
+++ backend/src/main/java/com/meong9/backend/domain/puppy/entity/Breed.java
@@ -1,21 +1,22 @@
 package com.meong9.backend.domain.puppy.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Getter
 public class Breed {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long breedId;
 
     @Column(name = "breed_name")
     private String name;
 
-    private boolean isFrequented;
+    private Boolean isFrequented;
 }
