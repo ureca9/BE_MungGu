79200eb00e1abcd0477188746ea65e3b6056af51
kuma0112 <lsj23871@gmail.com>
2024-11-24 17:00:52 +0900

    feat: 멤버 선호시설추가 기능 생성

--- backend/src/main/java/com/meong9/backend/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/meong9/backend/domain/member/controller/MemberController.java
@@ -1,8 +1,10 @@
 package com.meong9.backend.domain.member.controller;
 
+import com.meong9.backend.domain.member.dto.InterestDto;
 import com.meong9.backend.domain.member.dto.LoginResponseDto;
 import com.meong9.backend.domain.member.service.KakaoService;
 import com.meong9.backend.domain.member.service.MemberService;
+import com.meong9.backend.global.auth.entity.MemberDetails;
 import com.meong9.backend.global.auth.utils.JwtProvider;
 import com.meong9.backend.global.dto.CommonResponse;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -17,7 +20,7 @@ import java.util.Map;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class MemberController {
     private final KakaoService kakaoService;
@@ -26,7 +29,7 @@ public class MemberController {
     /**
      * 카카오 로그인 처리 컨트롤러
      */
-    @PostMapping("/callback/kakao")
+    @PostMapping("/auth/callback/kakao")
     public ResponseEntity<?> kakaoLogin(@RequestParam(name = "code") String code, HttpServletResponse response) throws IOException {
         LoginResponseDto dto = kakaoService.kakaoLogin(code, response);
         return CommonResponse.ok("success", dto);
@@ -35,7 +38,7 @@ public class MemberController {
     /**
      * access token 재발급 요청 처리 컨트롤러
      */
-    @PostMapping("/token")
+    @PostMapping("/auth/token")
     public ResponseEntity<?> refreshAccessToken(@RequestHeader("Refresh-Token") String refreshToken) {
         String newAccessToken = memberService.refreshAccessToken(refreshToken);
         HttpHeaders headers = new HttpHeaders();
@@ -43,5 +46,12 @@ public class MemberController {
         return ResponseEntity.ok()
                 .headers(headers)
                 .body(Map.of("message", "Access Token이 재발급되었습니다."));
+    }
+
+    @PostMapping("/members/interests")
+    public ResponseEntity<?> insertInterests(@RequestBody InterestDto dto,
+                                             @AuthenticationPrincipal MemberDetails memberDetails) {
+        memberService.insertInterests(dto, memberDetails.member());
+        return CommonResponse.ok("success");
     }
 }
--- backend/src/main/java/com/meong9/backend/domain/member/service/MemberService.java
+++ backend/src/main/java/com/meong9/backend/domain/member/service/MemberService.java
@@ -1,7 +1,12 @@
 package com.meong9.backend.domain.member.service;
 
+import com.meong9.backend.domain.member.dto.InterestDto;
 import com.meong9.backend.domain.member.entity.Member;
 import com.meong9.backend.domain.member.repository.MemberRepository;
+import com.meong9.backend.domain.member.repository.PlcFavCategoryJdbcRepositoryImpl;
+import com.meong9.backend.domain.member.repository.PlcFavCategoryRepository;
+import com.meong9.backend.domain.place.entity.PlcCategory;
+import com.meong9.backend.domain.place.repository.PlcCategoryRepository;
 import com.meong9.backend.global.auth.refreshtoken.RefreshToken;
 import com.meong9.backend.global.auth.refreshtoken.RefreshTokenService;
 import com.meong9.backend.global.auth.utils.JwtProvider;
@@ -9,16 +14,28 @@ import com.meong9.backend.global.exception.AuthenticationException;
 import com.meong9.backend.global.exception.NotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
+@Slf4j(topic = "MemberService")
 public class MemberService {
     private final MemberRepository memberRepository;
     private final JwtProvider jwtProvider;
     private final RefreshTokenService refreshTokenService;
+    private final PlcCategoryRepository plcCategoryRepository;
+    private final PlcFavCategoryRepository plcFavCategoryRepository;
+    private final PlcFavCategoryJdbcRepositoryImpl plcFavCategoryRepositoryImpl;
 
+    /**
+     * refresh token 사용하여 access token 재발급하는 서비스 메서드
+     */
     public String refreshAccessToken(String refreshToken) {
         jwtProvider.validateToken(refreshToken);
         String email = jwtProvider.getSubjectFromToken(refreshToken);
@@ -28,7 +45,6 @@ public class MemberService {
 
         if (!refreshTokenService.validateRefreshToken(email, refreshToken.substring(7))) {
             refreshTokenService.removeRefreshToken(storedRefreshToken);
-            System.out.println("bearer 없고2");
             throw AuthenticationException.unauthenticatedToken(refreshToken);
         }
 
@@ -36,5 +52,27 @@ public class MemberService {
                 .orElseThrow(() -> NotFoundException.entityNotFound("멤버"));
 
         return jwtProvider.createAccessToken(email, member.getRoleCode());
+    }
+
+    /**
+     * 사용자의 선호 시설을 저장하는 서비스 메서드
+     */
+    public void insertInterests(InterestDto dto, Member member) {
+        if (dto.getPlaces() == null || dto.getPlaces().isEmpty()) return;
+
+        Set<String> newCategoryNames = dto.getPlaces();
+        plcFavCategoryRepository.deleteByMemberId(member.getMemberId());
+
+        List<PlcCategory> categoriesToInsert = plcCategoryRepository.findAllByNameIn(newCategoryNames);
+
+        if (categoriesToInsert.size() != newCategoryNames.size()) {
+            throw NotFoundException.entityNotFound("일부 카테고리");
+        }
+
+        List<Object[]> batchParams = categoriesToInsert.stream()
+                .map(category -> new Object[]{member.getMemberId(), category.getPlcCategoryId()})
+                .collect(Collectors.toList());
+
+        plcFavCategoryRepositoryImpl.batchInsert(batchParams);
     }
 }
