28df670744178b3a48132732bd7e558034f0a206
hh830 <jhh8890@naver.com>
2024-11-27 20:00:15 +0900

    feat: mahout 추천

--- backend/src/main/java/com/meong9/backend/domain/recommendation/config/DataModelConfig.java
+++ backend/src/main/java/com/meong9/backend/domain/recommendation/config/DataModelConfig.java
@@ -24,25 +24,25 @@ public class DataModelConfig {
                 new MySQLJDBCDataModel(
                         dataSource,
                         "pension_member_score", // 테이블 이름
-                        "member_id",            // 회원 ID 컬럼
                         "pension_id",           // 펜션 ID 컬럼
+                        "member_id",            // 회원 ID 컬럼
                         "score",                // 점수 컬럼
-                        "timestamp"      // 타임스탬프 컬럼
+                        "last_updated_at"      // 타임스탬프 컬럼
                 )
         );
     }
 
-    // 사용자-시설 데이터 모델 (시설 추천)
+    // 펜션-시설 데이터 모델 (시설 추천)
     @Bean(name = "placeDataModel")
     public DataModel facilityDataModel() throws Exception {
         return new ReloadFromJDBCDataModel(
                 new MySQLJDBCDataModel(
                         dataSource,
-                        "place_member_score", // 테이블 이름
-                        "member_id",                  // 회원 ID 컬럼
+                        "pension_place_score", // 테이블 이름
+                        "pension_id",                  // 펜션 ID 컬럼
                         "place_id",                   // 시설 ID 컬럼
                         "score",                   // 점수 컬럼
-                        "timestamp" // 타임스탬프
+                        "last_updated_at" // 타임스탬프
                 )
         );
     }
--- backend/src/main/java/com/meong9/backend/domain/recommendation/recommendation/service/UserBasedRecommendation.java
+++ backend/src/main/java/com/meong9/backend/domain/recommendation/recommendation/service/UserBasedRecommendation.java
@@ -1,5 +1,6 @@
-package com.meong9.backend.domain.recommendation.service;
+package com.meong9.backend.domain.recommendation.recommendation.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
 import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
 import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
@@ -7,25 +8,36 @@ import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import org.apache.mahout.cf.taste.similarity.UserSimilarity;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
+@Slf4j
 public class UserBasedRecommendation {
 
     public List<RecommendedItem> recommend(DataModel dataModel, long userId, int numRecommendations) throws Exception {
-        // 사용자 간 유사도 계산
+        // 사용자 간 유사도 계산 - 피어슨 상관계수로 유사도 계산(-1~1)
         UserSimilarity similarity = new PearsonCorrelationSimilarity(dataModel);
 
-        // 가장 가까운 사용자 이웃 선택 (5명)
+        // 가장 가까운 사용자 이웃 선택 (num 명)
         UserNeighborhood neighborhood = new NearestNUserNeighborhood(numRecommendations, similarity, dataModel);
 
-        // 추천 엔진 생성
         GenericUserBasedRecommender recommender = new GenericUserBasedRecommender(dataModel, neighborhood, similarity);
 
         // 사용자 추천
-        return recommender.recommend(userId, numRecommendations);
+        List<RecommendedItem> recommendations = recommender.recommend(userId, numRecommendations);
+
+        // 추천 결과 로그 출력
+        if (recommendations.isEmpty()) {
+            log.info("No recommendations available for userId: {}", userId);
+        } else {
+            log.info("Recommendations for userId {}: ", userId);
+            recommendations.forEach(item -> log.info("Item ID: {}, Score: {}", item.getItemID(), item.getValue()));
+        }
+
+        return recommendations;
     }
 }
