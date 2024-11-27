42a58a2ba1c3d0916c8034fa31974e7ff7cc7ac0
hh830 <jhh8890@naver.com>
2024-11-27 20:00:15 +0900

    feat: mahout 추천

--- backend/src/main/java/com/meong9/backend/domain/recommendation/recommendation/service/ItemBasedRecommendation.java
+++ backend/src/main/java/com/meong9/backend/domain/recommendation/recommendation/service/ItemBasedRecommendation.java
@@ -1,6 +1,7 @@
-package com.meong9.backend.domain.recommendation.service;
+package com.meong9.backend.domain.recommendation.recommendation.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.mahout.cf.taste.impl.recommender.GenericItemBasedRecommender;
 import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
@@ -8,20 +9,29 @@ import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class ItemBasedRecommendation {
 
-    public List<RecommendedItem> recommend(DataModel dataModel, long facilityId, int numRecommendations) throws Exception {
+    public List<RecommendedItem> recommend(DataModel dataModel, long pensionId, int numRecommendations) throws Exception {
         // 시설 간 유사도 계산
         ItemSimilarity similarity = new PearsonCorrelationSimilarity(dataModel);
-        // Item-Based 추천 생성
+        // Item-Based 객체 초기화
         GenericItemBasedRecommender recommender = new GenericItemBasedRecommender(dataModel, similarity);
+        // 추천 생성
+        List<RecommendedItem> recommendations = recommender.recommend(pensionId, numRecommendations);
 
-        return recommender.recommend(facilityId, numRecommendations);
+        log.info("item base 추천 pensionId: {}", pensionId);
+        recommendations.forEach(item -> log.info("추천된 place ID: {}, score: {}", item.getItemID(), item.getValue()));
+
+        return recommendations;
+
+
     }
 }
 
