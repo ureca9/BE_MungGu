d3f426271499a227904389f278d463a86cfa2d9d
hh830 <jhh8890@naver.com>
2024-11-28 11:19:23 +0900

    feat: 리뷰 엔티티 수정 관련

--- backend/src/main/java/com/meong9/backend/domain/review/repository/ReviewRepository.java
+++ backend/src/main/java/com/meong9/backend/domain/review/repository/ReviewRepository.java
@@ -2,6 +2,13 @@ package com.meong9.backend.domain.review.repository;
 
 import com.meong9.backend.domain.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    @Query("SELECT r.placePensionId FROM Review r where r.type = '020' GROUP BY r.placePensionId HAVING COUNT(r.reviewId) > :count")
+    List<Long> findPensionReviewCount(@Param("count") int count);
+
 }
