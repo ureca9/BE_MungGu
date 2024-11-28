9271a4ad9033de9eef8b2769825d9a1160380689
kuma0112 <lsj23871@gmail.com>
2024-11-28 10:23:48 +0900

    refactor: ignoredRequests 리스트에서 /api/v1/auth/token 삭제

--- backend/src/main/java/com/meong9/backend/global/auth/config/SecurityConfig.java
+++ backend/src/main/java/com/meong9/backend/global/auth/config/SecurityConfig.java
@@ -60,7 +60,6 @@ public class SecurityConfig {
         // 필터링에서 제외할 요청들
         final RequestMatcher ignoredRequests = new OrRequestMatcher(
                 List.of(new AntPathRequestMatcher("/api/v1/auth/callback/kakao", HttpMethod.POST.name()),
-                        new AntPathRequestMatcher("/api/v1/auth/token", HttpMethod.POST.name()),
                         new AntPathRequestMatcher("/api/v1/members/check", HttpMethod.GET.name()),
                         new AntPathRequestMatcher("/api/v1/searches/places", HttpMethod.GET.name()),
                         new AntPathRequestMatcher("/api/v1/searches/pensions", HttpMethod.GET.name()),
