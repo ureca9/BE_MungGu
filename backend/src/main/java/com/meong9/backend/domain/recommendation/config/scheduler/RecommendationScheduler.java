420eef0e16a4222c73063e8d62da3cef148d77d4
hh830 <jhh8890@naver.com>
2024-11-28 11:19:35 +0900

    feat: 배치 작업

--- backend/src/main/java/com/meong9/backend/domain/recommendation/config/scheduler/RecommendationScheduler.java
+++ backend/src/main/java/com/meong9/backend/domain/recommendation/config/scheduler/RecommendationScheduler.java
@@ -3,6 +3,7 @@ package com.meong9.backend.domain.recommendation.config.scheduler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -23,7 +24,10 @@ public class RecommendationScheduler {
     @Scheduled(cron = "0 */1 * * * ?") // 1분마다
     public void runRecommendationBatch() {
         try {
-            jobLauncher.run(recommendationJob, new JobParameters());
+            JobParameters jobParameters = new JobParametersBuilder()
+                .addLong("time", System.currentTimeMillis())
+                .toJobParameters();
+            jobLauncher.run(recommendationJob, jobParameters);
         } catch (Exception e) {
             e.printStackTrace();
         }
