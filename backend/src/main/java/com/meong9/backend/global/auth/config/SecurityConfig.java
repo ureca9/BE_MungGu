df64b52423b872d8fa61ae3398c73af5e5b5bac0
hh830 <jhh8890@naver.com>
2024-11-30 01:45:13 +0900

    fix: 실시간 추천 url 필터링 제외 항목에 추가

--- backend/src/main/java/com/meong9/backend/global/auth/config/SecurityConfig.java
+++ backend/src/main/java/com/meong9/backend/global/auth/config/SecurityConfig.java
@@ -64,6 +64,7 @@ public class SecurityConfig {
                 List.of(new AntPathRequestMatcher("/api/v1/auth/callback/kakao", HttpMethod.GET.name()),
                         new AntPathRequestMatcher("/api/v1/members/check", HttpMethod.GET.name()),
                         new AntPathRequestMatcher("/api/v1/spots/rankings", HttpMethod.GET.name()),
+                        new AntPathRequestMatcher("/api/v1/spots/reviews", HttpMethod.GET.name()),
                         new AntPathRequestMatcher("/api/v1/pensions/{pensionId}", HttpMethod.GET.name()),
                         new AntPathRequestMatcher("/api/v1/pensions/{placeId}", HttpMethod.GET.name()),
                         new AntPathRequestMatcher("/api/v1/pensions/{placeId}/reviews", HttpMethod.GET.name()),
