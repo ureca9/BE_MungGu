1aa449b462cb5bf42a6630fd5d33cd904780583f
hh830 <jhh8890@naver.com>
2024-11-30 01:43:03 +0900

    feat: 실시간 리뷰 조회

--- backend/src/main/java/com/meong9/backend/domain/address/repository/PlcPenAddressRepository.java
+++ backend/src/main/java/com/meong9/backend/domain/address/repository/PlcPenAddressRepository.java
@@ -5,7 +5,22 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface PlcPenAddressRepository extends JpaRepository<PlcPenAddress, Long> {
     @Query("select ppa from PlcPenAddress ppa where ppa.plcPenId =:plcPenId and ppa.type =:type")
     PlcPenAddress findByPlcPenIdAndType(@Param("plcPenId")Long plcPenId, @Param("type")String type);
+
+
+
+    @Query("""
+        SELECT pa FROM PlcPenAddress pa
+        LEFT JOIN FETCH pa.address a
+        WHERE (pa.plcPenId IN :pensionIds AND pa.type = '020') 
+           OR (pa.plcPenId IN :placeIds AND pa.type = '010')
+    """)
+    List<PlcPenAddress> findByPlcPenIdInAndType(
+            @Param("pensionIds") List<Long> pensionIds,
+            @Param("placeIds") List<Long> placeIds
+    );
 }
