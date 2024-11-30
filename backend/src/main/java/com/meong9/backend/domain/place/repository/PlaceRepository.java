5f8351c7249efe424e9f191564c325380b204b15
kuma0112 <lsj23871@gmail.com>
2024-11-30 17:46:50 +0900

    refactor: 검색 기존 쿼리 삭제

--- backend/src/main/java/com/meong9/backend/domain/place/repository/PlaceRepository.java
+++ backend/src/main/java/com/meong9/backend/domain/place/repository/PlaceRepository.java
@@ -13,30 +13,5 @@ import java.util.List;
 
 @Repository
 public interface PlaceRepository extends JpaRepository<Place, Long> {
-    @Query("""
-            SELECT new com.meong9.backend.domain.search.dto.SearchPlaceDto(
-            p.placeId,
-            p.name,
-            NULL,
-            p.plcCategory.name,
-            NULL,
-            p.reviewAvg,
-            p.reviewCount,
-            p.enterPetSize,
-            p.businessHour,
-            NULL,
-            NULL)
-            FROM Place p
-            WHERE p.placeId IN :regionCondition
-            AND p.plcCategory.plcCategoryId IN :categoryCondition
-            AND (p.enterPetSize = '030' OR
-            (p.enterPetSize = '020' AND :sizeCode IN ('020', '010')) OR
-            (p.enterPetSize = '010' AND :sizeCode = '010'))
-            """)
-    Slice<SearchPlaceDto> findPlacesByCondition(@Param("regionCondition") List<Long> regionCondition,
-                                                   @Param("categoryCondition") List<Long> categoryCondition,
-                                                   @Param("sizeCode") String sizeCode,
-                                                   Pageable pageable);
-
 
 }
