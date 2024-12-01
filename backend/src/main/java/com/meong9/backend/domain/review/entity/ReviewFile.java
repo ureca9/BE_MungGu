833b1fb6d65adf9a145aeb7cba6963a415ecec3e
shkum0330 <shkum0330@gmail.com>
2024-12-01 17:57:59 +0900

    refactor: 리뷰-리뷰파일 복합키로 변경

--- backend/src/main/java/com/meong9/backend/domain/review/entity/ReviewFile.java
+++ backend/src/main/java/com/meong9/backend/domain/review/entity/ReviewFile.java
@@ -4,20 +4,28 @@ import com.meong9.backend.global.entity.BaseTimeEntity;
 import com.meong9.backend.global.mediafile.entity.MediaFile;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@IdClass(ReviewFileId.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class ReviewFile extends BaseTimeEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id; // 후기 파일 아이디
+public class ReviewFile{ // 복합 키 클래스
+    @EmbeddedId
+    private ReviewFileId id; // 복합 키
+
+    @ManyToOne
+    @MapsId("reviewId") // 복합 키와 매핑
+    @JoinColumn(name = "review_id", nullable = false)
+    private Review review;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("mediaFileId") // 복합 키와 매핑
     @JoinColumn(name = "media_file_id", nullable = false)
-    private MediaFile file; // 연관된 파일
+    private MediaFile file;
+
 
     public ReviewFile(MediaFile file) {
         this.file = file;
