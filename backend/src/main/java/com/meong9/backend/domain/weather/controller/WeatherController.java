ea18987b42c01654386bf67574a05059d8a6eba6
hh830 <jhh8890@naver.com>
2024-12-02 13:04:24 +0900

    feat: 기상청 api 호출

---
+++ backend/src/main/java/com/meong9/backend/domain/weather/controller/WeatherController.java
@@ -0,0 +1,18 @@
+package com.meong9.backend.domain.weather.controller;
+
+import com.meong9.backend.domain.weather.service.WeatherService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class WeatherController {
+
+    private final WeatherService weatherService;
+
+    @GetMapping("/weather")
+    public String getWeather() {
+        return weatherService.getWeatherForecast();
+    }
+}
