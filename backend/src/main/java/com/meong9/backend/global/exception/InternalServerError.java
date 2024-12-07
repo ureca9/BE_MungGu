f0ef3b5afb3de9fe3e852c3343534dd59e2001e6
hh830 <jhh8890@naver.com>
2024-12-07 13:40:20 +0900

    feat: 예외처리

--- backend/src/main/java/com/meong9/backend/global/exception/InternalServerError.java
+++ backend/src/main/java/com/meong9/backend/global/exception/InternalServerError.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 
 public class InternalServerError extends BaseException {
     static private final String PHOTO_PROCESSING_ERROR = "S3에서 사진 저장 중 문제가 발생했습니다.";
+    static private final String WEATHER_API_ERROR = "기상청 api 호출 중 문제가 발생했습니다. %s";
 
     public InternalServerError(String message) {
         super(HttpStatus.INTERNAL_SERVER_ERROR, message);
@@ -12,4 +13,9 @@ public class InternalServerError extends BaseException {
     public static InternalServerError photoProcessingError() {
         return new InternalServerError(PHOTO_PROCESSING_ERROR);
     }
+
+    public static InternalServerError weatherApiError(String message){
+        return new InternalServerError(String.format(WEATHER_API_ERROR, message));
+    }
+
 }
