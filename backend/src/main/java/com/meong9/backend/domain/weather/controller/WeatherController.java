5962fd4eb9c75604f0da7f28b543a2058fa16af4
hh830 <jhh8890@naver.com>
2024-12-07 13:40:03 +0900

    feat: 기상청 api 호출 및 레디스 저장, 조회

--- backend/src/main/java/com/meong9/backend/domain/weather/controller/WeatherController.java
+++ backend/src/main/java/com/meong9/backend/domain/weather/controller/WeatherController.java
@@ -1,23 +1,33 @@
 package com.meong9.backend.domain.weather.controller;
 
+import com.meong9.backend.domain.weather.dto.WeatherDto;
+import com.meong9.backend.domain.weather.service.WeatherApiService;
 import com.meong9.backend.domain.weather.service.WeatherService;
+import com.meong9.backend.global.dto.CommonResponse;
+import com.meong9.backend.global.exception.BadRequestException;
+import com.meong9.backend.global.utils.RegionMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class WeatherController {
 
     private final WeatherService weatherService;
 
     @GetMapping("/weather")
-    public String getWeather() {
-        return weatherService.getWeatherForecast();
-    }
+    public ResponseEntity<?> getWeather(@RequestParam(name = "region") String region){
+        String regionName = RegionMapper.getWeatherRegion(region); // 지역 코드
+        if(regionName == null){
+            throw BadRequestException.invalidRegionNameFormat(region);
+        }
+        WeatherDto weatherDto = weatherService.getWeatherData(region);
 
-    @GetMapping("/weather/2")
-    public String getWeather2() {
-        return weatherService.getWeatherForecast2();
+        return CommonResponse.ok("success", weatherDto);
     }
 }
