02df0ef1da7a0c49c12798c78c0ac481a1a7aa5b
kuma0112 <lsj23871@gmail.com>
2024-12-13 14:34:24 +0900

    chore: unused import문 삭제

--- backend/src/main/java/com/meong9/backend/domain/meongPhoto/service/MeongPhotoService.java
+++ backend/src/main/java/com/meong9/backend/domain/meongPhoto/service/MeongPhotoService.java
@@ -22,7 +22,6 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
