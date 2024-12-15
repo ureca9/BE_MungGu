016d41e43f1d592548840bfa7c4e3c251936a9e6
kuma0112 <lsj23871@gmail.com>
2024-12-15 23:17:18 +0900

    refactor: security config 수정 및 login url 변경

--- backend/src/main/java/com/meong9/backend/global/auth/config/SecurityConfig.java
+++ backend/src/main/java/com/meong9/backend/global/auth/config/SecurityConfig.java
@@ -66,19 +66,16 @@ public class SecurityConfig {
                         new AntPathRequestMatcher("/api/v1/members/check", HttpMethod.GET.name()),
                         new AntPathRequestMatcher("/api/v1/spots/rankings", HttpMethod.GET.name()),
                         new AntPathRequestMatcher("/api/v1/spots/reviews", HttpMethod.GET.name()),
-                        new AntPathRequestMatcher("/api/v1/pensions/detail/{pensionId}", HttpMethod.GET.name()),
                         new AntPathRequestMatcher("/api/v1/pensions/{pensionId}/recommendations", HttpMethod.GET.name()),
                         new AntPathRequestMatcher("/api/v1/{pensionId}/rooms", HttpMethod.GET.name()),
                         new AntPathRequestMatcher("/api/v1/pensions/{pensionId}/reviews", HttpMethod.GET.name()),
                         new AntPathRequestMatcher("/api/v1/pensions/{pensionId}/summary", HttpMethod.GET.name()),
-                        new AntPathRequestMatcher("/api/v1/places/detail/{placeId}", HttpMethod.GET.name()),
                         new AntPathRequestMatcher("/api/v1/places/{placeId}/reviews", HttpMethod.GET.name()),
                         new AntPathRequestMatcher("/api/v1/places/{placeId}/summary", HttpMethod.GET.name()),
                         new AntPathRequestMatcher("/api/v1/map/places", HttpMethod.GET.name()),
                         new AntPathRequestMatcher("/api/v1/photos", HttpMethod.GET.name()),
                         new AntPathRequestMatcher("/api/v1/places/{category}/top", HttpMethod.GET.name()),
                         new AntPathRequestMatcher("/api/v1/pensions/top", HttpMethod.GET.name()),
-                        new AntPathRequestMatcher("/", HttpMethod.GET.name()),
                         new AntPathRequestMatcher("/api/v1/weather", HttpMethod.GET.name()),
 
                         new AntPathRequestMatcher("/actuator/health", HttpMethod.GET.name()),
@@ -89,7 +86,9 @@ public class SecurityConfig {
         http.authorizeHttpRequests(auth -> auth
                 .requestMatchers(ignoredRequests).permitAll()
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // 정적 리소스 허용
-                .requestMatchers(HttpMethod.GET, "/api/v1/search/**", "/api/v1/spots/recommendations", "/api/v1/map/search").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/v1/search/**", "/api/v1/spots/recommendations",
+                        "/api/v1/map/search", "/api/v1/pensions/detail/{pensionId}",
+                        "/api/v1/places/detail/{placeId}").permitAll()
                 .requestMatchers("/index.html", "/favicon.ico").permitAll()
                 .requestMatchers(HttpMethod.GET, "/ping", "/error", "/actuator/health").permitAll() // 헬스 체크 허용
                 .anyRequest().authenticated() // 나머지 요청은 MEMBER 역할 필요
