662a6632931cee3e343680fb39a4dea5569c15b8
kuma0112 <lsj23871@gmail.com>
2024-12-18 14:15:50 +0900

    refactor: 핫한 장소 조회 중복 결과 반환 수정

--- backend/src/main/java/com/meong9/backend/domain/place/dto/TopPlaceResponseDto.java
+++ backend/src/main/java/com/meong9/backend/domain/place/dto/TopPlaceResponseDto.java
@@ -3,39 +3,41 @@ package com.meong9.backend.domain.place.dto;
 import com.meong9.backend.global.utils.AddressMapper;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class TopPlaceResponseDto {
-    private final Long placeId;
-    private final String placeName;
-    private final Integer reviewCount;
-    private final BigDecimal reviewAvg;
-    private final String province;
-    private final String cityDistrict;
-    private final String subDistrict;
-    private final Long viewCount;
-    private final String address;
+    private Long placeId;
+    private String placeName;
+    private Integer reviewCount;
+    private BigDecimal reviewAvg;
+    private String province;
+    private String cityDistrict;
+    private String subDistrict;
+    private String address;
+    private Long totalViewCount;
 
     @Setter
     private String placeImageUrl;
 
-    public TopPlaceResponseDto(Long placeId, String placeName, Integer reviewCount, BigDecimal reviewAvg, String province, String cityDistrict, String subDistrict, Long viewCount) {
+    public TopPlaceResponseDto(Long placeId, String placeName, Integer reviewCount, Double reviewAvg,
+                               String province, String cityDistrict, String subDistrict, Long totalViewCount) {
         this.placeId = placeId;
         this.placeName = placeName;
         this.reviewCount = reviewCount;
-        this.reviewAvg = reviewAvg;
+        this.reviewAvg = reviewAvg != null ? BigDecimal.valueOf(reviewAvg) : null;
         this.province = province;
         this.cityDistrict = cityDistrict;
         this.subDistrict = subDistrict;
-        this.viewCount = viewCount;
+        this.totalViewCount = totalViewCount;
         this.address = AddressMapper.formatAddress(province, cityDistrict, subDistrict);
     }
 
-
-
-
 }
+
+
