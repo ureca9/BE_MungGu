93112f3773ab2477b812e850b31e6ed817f39e0e
kuma0112 <lsj23871@gmail.com>
2025-01-18 15:26:18 +0900

    refactor: uploadHlsToS3() 생성 및 사용하지 않는 메서드 삭제

--- backend/src/main/java/com/meong9/backend/global/mediafile/service/MediaFileService.java
+++ backend/src/main/java/com/meong9/backend/global/mediafile/service/MediaFileService.java
@@ -374,23 +374,6 @@ public class MediaFileService {
         );
     }
 
-    public String getResizeBucketUrl(String fileKey) {
-        return String.format("https://%s.s3.%s.amazonaws.com/%s", resizeBucket, region, fileKey);
-    }
-
-    public MediaFile registerFileKey(String fileKey) {
-
-        String fileUrl = String.format("https://%s.s3.%s.amazonaws.com/%s", bucket, region, fileKey);
-
-        return MediaFile.builder()
-                .fileType(FileType.IMAGE)
-                .fileKey(fileKey)
-                .fileName("profile.jpg")
-                .fileUrl(fileUrl)
-                .build();
-    }
-
-
     public Map<String, String> getPresingedUrl(String objectKey) {
         // PreSigned URL 생성
         Date expiration = new Date(System.currentTimeMillis() + 1000 * 60 * 10); // 10분 유효
@@ -426,21 +409,27 @@ public class MediaFileService {
         return amazonS3.generatePresignedUrl(presignedUrlRequest);
     }
 
-    public void uploadVideoAndThumbnail(String localFilePath, String s3Key) {
-        try (FileInputStream fileInputStream = new FileInputStream(localFilePath)) {
-            ObjectMetadata metadata = new ObjectMetadata();
-            metadata.setContentLength(new File(localFilePath).length());
-            if (localFilePath.endsWith(".mp4")) {
-                metadata.setContentType("video/mp4");
-            } else if (localFilePath.endsWith(".jpg")) {
-                metadata.setContentType("image/jpeg");
-            }
+    /**
+     * 트랜스코딩된 파일을 S3에 업로드
+     */
+    public void uploadHlsToS3(String localDirectory, String s3Directory) {
+        File dir = new File(localDirectory);
+        if (!dir.isDirectory()) {
+            throw new IllegalArgumentException("로컬 경로가 디렉토리가 아닙니다: " + localDirectory);
+        }
 
-            s3Client.putObject(bucket, s3Key, fileInputStream, metadata);
-            String s3Url = s3Client.getUrl(bucket, s3Key).toString();
-            System.out.println("S3 업로드 완료: " + s3Url);
-        } catch (IOException e) {
-            throw new RuntimeException("S3 업로드 중 오류 발생: " + e.getMessage(), e);
+        for (File file : dir.listFiles()) {
+            String s3Key = s3Directory + "/" + file.getName();
+            try (FileInputStream fileInputStream = new FileInputStream(file)) {
+                ObjectMetadata metadata = new ObjectMetadata();
+                metadata.setContentLength(file.length());
+                metadata.setContentType(file.getName().endsWith(".m3u8") ? "application/vnd.apple.mpegurl" : "video/MP2T");
+
+                s3Client.putObject(bucket, s3Key, fileInputStream, metadata);
+                System.out.println("S3 업로드 완료: " + s3Key);
+            } catch (IOException e) {
+                throw new RuntimeException("S3 업로드 중 오류 발생: " + e.getMessage(), e);
+            }
         }
     }
 }
