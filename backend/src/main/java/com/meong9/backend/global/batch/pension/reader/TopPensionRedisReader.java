de960ee69746e9cf6017d33e55215578bb1361f4
PiggPotato <macalop69@gmail.com>
2025-01-18 16:17:59 +0900

    refactor: 펜션 조회수 redis 저장 key값 변경 및 주간 조회수 저장 로직 추가 ex) pension:weekly:viewCount2025-01-14, pension:viewCount:2025-01-14

--- backend/src/main/java/com/meong9/backend/global/batch/pension/reader/TopPensionRedisReader.java
+++ backend/src/main/java/com/meong9/backend/global/batch/pension/reader/TopPensionRedisReader.java
@@ -6,6 +6,7 @@ import com.meong9.backend.domain.pension.entity.Pension;
 import com.meong9.backend.domain.pension.repository.PensionRepository;
 import com.meong9.backend.domain.pension.service.RoomService;
 import com.meong9.backend.global.batch.pension.dto.RedisTopPensionDto;
+import com.meong9.backend.global.utils.RedisUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.ItemReader;
@@ -44,7 +45,7 @@ public class TopPensionRedisReader implements ItemReader<RedisTopPensionDto> {
     private void loadNextPage() {
         // Step 1: Redis에서 데이터 조회
         Set<ZSetOperations.TypedTuple<String>> redisData = redisTemplate.opsForZSet()
-                .reverseRangeWithScores("pension:viewCount", currentIndex, currentIndex + PAGE_SIZE - 1);
+                .reverseRangeWithScores("pension:viewCount:" + RedisUtils.formatRelativeToNowDate(1), currentIndex, currentIndex + PAGE_SIZE - 1);
 
         if (redisData == null || redisData.isEmpty()) {
             iterator = null;
--- backend/src/main/java/com/meong9/backend/global/utils/RedisUtils.java
+++ backend/src/main/java/com/meong9/backend/global/utils/RedisUtils.java
@@ -1,13 +1,34 @@
 package com.meong9.backend.global.utils;
 
 import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 
 public class RedisUtils {
+
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     public static long calculateTTLUntil2AM() {
         LocalDateTime now = LocalDateTime.now();
         LocalDateTime twoAM = now.toLocalDate().atStartOfDay().plusDays(1).plusHours(2); // 다음 날 오전 2시
         return Duration.between(now, twoAM).getSeconds();
     }
+
+    // 현재 날짜 포맷팅
+    public static String formatCurrentDate() {
+        return LocalDate.now().format(DATE_FORMATTER);
+    }
+
+    // 특정 날짜 포맷팅
+    public static String formatDate(LocalDate date) {
+        return date.format(DATE_FORMATTER);
+    }
+
+    // n일 전 날짜 포멧팅
+    public static String formatRelativeToNowDate(int day){
+        return LocalDate.now().minusDays(day).format(DATE_FORMATTER);
+    }
+
 }
