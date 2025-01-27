b857a24f730b7dd1190434468e490e4fb1ccd1e0
kuma0112 <lsj23871@gmail.com>
2025-01-27 16:57:12 +0900

    refactor: kafka producer 재시도 및 타임아웃 설정 추가

--- backend/src/main/java/com/meong9/backend/global/kafka/config/KafkaProducerConfig.java
+++ backend/src/main/java/com/meong9/backend/global/kafka/config/KafkaProducerConfig.java
@@ -20,6 +20,9 @@ public class KafkaProducerConfig {
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.RETRIES_CONFIG, 3);
+        configProps.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, 5000);
+        configProps.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, 30000);
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
