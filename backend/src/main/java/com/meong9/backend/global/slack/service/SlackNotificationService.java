93ed764ed65d60bdf3eb36b4f41e875f45ba1028
kuma0112 <lsj23871@gmail.com>
2025-01-27 17:09:09 +0900

    refactor: Slack 알림 전송 재시도 로직 추가

--- backend/src/main/java/com/meong9/backend/global/slack/service/SlackNotificationService.java
+++ backend/src/main/java/com/meong9/backend/global/slack/service/SlackNotificationService.java
@@ -1,23 +1,32 @@
 package com.meong9.backend.global.slack.service;
 
+import com.meong9.backend.global.config.RateLimiterProvider;
 import com.meong9.backend.global.slack.entity.SlackNotificationType;
 import com.slack.api.Slack;
 import com.slack.api.model.block.Blocks;
 import com.slack.api.model.block.LayoutBlock;
 import com.slack.api.model.block.composition.BlockCompositions;
 import com.slack.api.webhook.WebhookPayloads;
+import io.github.resilience4j.ratelimiter.RateLimiter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 @Slf4j
 public class SlackNotificationService {
+
+    private final RateLimiter rateLimiter;
+
+    public SlackNotificationService(RateLimiterProvider rateLimiterProvider) {
+        this.rateLimiter = rateLimiterProvider.createSlackRateLimiter();
+    }
 
     @Value("${slack.webhook.url}")
     private String webhookUrl;
@@ -25,19 +34,37 @@ public class SlackNotificationService {
     private final StringBuilder sb = new StringBuilder();
 
     // Slack 알림 전송
+    @Retryable(
+            retryFor = IOException.class,
+            maxAttempts = 3,
+            backoff = @Backoff(delay = 1000)
+            )
     public void sendSlackNotification(SlackNotificationType type, String message) {
-        try {
-            // 메시지 내용 생성
-            List<LayoutBlock> layoutBlocks = generateLayoutBlock(type, message);
+        // 레이트 리미터를 사용하여 알림 전송
+        Runnable slackNotificationTask = RateLimiter.decorateRunnable(rateLimiter, () -> {
+            try {
+                // 메시지 내용 생성
+                List<LayoutBlock> layoutBlocks = generateLayoutBlock(type, message);
 
-            // Slack API 호출하여 메시지 전송
-            Slack.getInstance().send(webhookUrl, WebhookPayloads
-                    .payload(p -> p.username(type.getUsername())
-                            .iconUrl(type.getIconUrl())
-                            .blocks(layoutBlocks)));
-            log.info("Slack 알림 전송 성공: {}", message);
-        } catch (IOException e) {
-            log.error("Slack 알림 전송 실패: {}", e.getMessage(), e);
+                // Slack API 호출하여 메시지 전송
+                Slack.getInstance().send(
+                        webhookUrl,
+                        WebhookPayloads.payload(p -> p.username(type.getUsername())
+                                .iconUrl(type.getIconUrl())
+                                .blocks(layoutBlocks))
+                );
+            } catch (IOException e) {
+                log.error("Slack 알림 전송 실패: {}", message, e);
+                throw new RuntimeException("Slack 알림 전송 중 오류 발생", e);
+            }
+        });
+
+        // 알림 실행
+        try {
+            slackNotificationTask.run();
+        } catch (Exception e) {
+            log.error("Slack 알림 처리 중 오류 발생: {}", message, e);
+            throw e; // 상위 호출자에게 예외 전달
         }
     }
 
