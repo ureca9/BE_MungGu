5dbbe08915d9582b8cfafbcc6be0f5c724f79f64
kuma0112 <lsj23871@gmail.com>
2025-01-27 17:40:08 +0900

    refactor: Slack 알림 대기시간 및 url 수정

--- backend/src/main/java/com/meong9/backend/global/config/RateLimiterProvider.java
+++ backend/src/main/java/com/meong9/backend/global/config/RateLimiterProvider.java
@@ -10,7 +10,7 @@ import java.time.Duration;
 public class RateLimiterProvider {
     public RateLimiter createSlackRateLimiter() {
         RateLimiterConfig config = RateLimiterConfig.custom()
-                .timeoutDuration(Duration.ofMillis(100)) // 허용량 초과 시 대기 시간
+                .timeoutDuration(Duration.ofSeconds(1)) // 허용량 초과 시 대기 시간
                 .limitRefreshPeriod(Duration.ofSeconds(1)) // 제한 주기
                 .limitForPeriod(1) // 주기당 허용 요청 수
                 .build();
